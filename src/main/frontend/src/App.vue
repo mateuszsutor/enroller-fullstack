901985cf0963e2180938ffc408b9855145f31dc5
MateuszSutor <mateusz.sutor@icloud.com>
2020-05-17 14:16:33 +0200

    Adding register form

--- src/main/frontend/src/App.vue
+++ src/main/frontend/src/App.vue
@@ -1,19 +1,34 @@
 <template>
-  <div id="app">
-    <h1>
-      <img src="./assets/logo.svg" alt="Enroller" class="logo">
-      System do zapisów na zajęcia
-    </h1>
-    <div v-if="authenticatedUsername">
-      <h2>Witaj {{ authenticatedUsername }}!
-        <a @click="logout()" class="float-right  button-outline button">Wyloguj</a>
-      </h2>
-      <meetings-page :username="authenticatedUsername"></meetings-page>
+    <div id="app">
+        <h1>
+            <img src="./assets/logo.svg" alt="Enroller" class="logo">
+            System do zapisów na zajęcia
+        </h1>
+        <div v-if="authenticatedUsername">
+            <h2>Witaj {{ authenticatedUsername }}!
+                <a @click="logout()" class="float-right  button-outline button">Wyloguj</a>
+            </h2>
+            <meetings-page :username="authenticatedUsername"></meetings-page>
+        </div>
+        <div v-else>
+            <button @click="registering=false"
+                    :class="registering ?'button-outline' : ''">Loguje się
+            </button>
+
+            <button @click="registering=true"
+                    :class="!registering ?'button-outline' : ''">Rejestruje się
+            </button>
+
+            <div v-if="error" class="error-allert">{{ error }}</div>
+
+            <login-form @login="login($event)"
+                        v-if="registering == false"></login-form>
+
+            <login-form @login="register($event)"
+                        v-else
+                        button-label="Zarejestruj się"></login-form>
+        </div>
     </div>
-    <div v-else>
-      <login-form @login="login($event)"></login-form>
-    </div>
-  </div>
 </template>
 
 <script>
@@ -23,30 +38,52 @@
 
     export default {
         components: {LoginForm, MeetingsPage},
-        data() {
-            return {
-                authenticatedUsername: ""
-            };
-        },
-        methods: {
-            login(user) {
-                this.authenticatedUsername = user.login;
+
+            data() {
+                return {
+                    authenticatedUsername: "",
+                    registering: false,
+                    error: '',
+                };
             },
-            logout() {
-                this.authenticatedUsername = '';
+            methods: {
+                login(user) {
+                    this.authenticatedUsername = user.login;
+                },
+
+                register(user) {
+                    this.error ='';
+                    this.$http.post('participants', user)
+                        .then(response => {
+                            this.registering = false;
+                        })
+                        .catch(response => {
+                            this.error = "Nazwa uzytkownika jest zajęta"
+                        });
+                },
+
+                logout() {
+                    this.authenticatedUsername = '';
+                }
             }
-        }
-    };
+
+    }
 </script>
 
 <style>
-  #app {
-    max-width: 1000px;
-    margin: 0 auto;
-  }
+    #app {
+        max-width: 1000px;
+        margin: 0 auto;
+    }
 
-  .logo {
-    vertical-align: middle;
-  }
+    .logo {
+        vertical-align: middle;
+    }
+
+    .error-allert{
+        border: 3px dotted red;
+        padding: 10px;
+        text-align: center;
+    }
 </style>
 
